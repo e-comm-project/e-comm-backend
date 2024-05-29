38d2a4e2a775a4c4d7dbce11c9228c1731606fee
Richard Hadzhiev <ri4koto100@gmail.com>
2024-05-29 18:04:36 +0200

    Implement order creation, update, and deletion routes in order.routes.js

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -21,4 +21,85 @@ router.get("/orders/:id", isAuthenticated, (req, res, next) => {
     });
 });
 
+router.post("/orders", isAuthenticated, (req, res, next) => {
+  const userId = req.payload._id;
+  const { products } = req.body; // products should be an array of product IDs
+  const total = products.reduce(
+    (acc, product) => acc + product.priceAtPurchase,
+    0
+  );
+  // check if the user already has an order  to add a new product inside the products array
+  // Create a new order
+  const newOrder = new Order({
+    user: userId,
+    products: products.map((product) => ({
+      product: product.product, // Ensure product is an ObjectId
+      quantity: product.quantity,
+      priceAtPurchase: product.priceAtPurchase,
+    })),
+    total,
+  });
+  // Save the new order to the database
+  newOrder
+    .save()
+    .then((order) => {
+      res.status(201).json(order);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// {
+//   $push: { products: { $each: newProducts } },
+//   $inc: { total: total },
+// },
+// { new: true }
+
+router.put("/orders/:id", isAuthenticated, (req, res, next) => {
+  const orderId = req.params.id;
+  const { product, quantity, priceAtPurchase } = req.body;
+
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return res.status(404).json({ message: "Order not found" });
+      }
+
+      // Create a new product object
+      const newProduct = {
+        product: product,
+        quantity: quantity,
+        priceAtPurchase: priceAtPurchase,
+      };
+
+      // Add the new product to the order's products array
+      order.products.push(newProduct);
+
+      // Recalculate the total price based on the updated products
+      order.total += quantity * priceAtPurchase;
+
+      // Save the updated order to the database
+      return order.save();
+    })
+    .then((updatedOrder) => {
+      res.status(200).json(updatedOrder);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.delete("/orders/:id", isAuthenticated, (req, res, next) => {
+  const orderId = req.params.id;
+  const userId = req.payload._id;
+  Order.findOneAndDelete({ _id: orderId, user: userId })
+    .then((order) => {
+      res.status(200).json(order);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 module.exports = router;
