6270afde65c61171039c8715ba0f57b4a24dd075
Richard Hadzhiev <ri4koto100@gmail.com>
2024-05-29 11:13:10 +0200

    implement orderRoutes

---
+++ routes/order.routes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const Order = require("../models/Order.model");
+const router = express.Router();
+const isAuthenticated = require("../middleware/isAuthenticated");
+
+router.get("/orders/:id", isAuthenticated, (req, res, next) => {
+  const userId = req.params.id;
+  const authenticatedUserId = req.payload._id;
+  if (userId !== authenticatedUserId) {
+    return res.status(403).json({ message: "Unauthorized access" });
+  }
+  Order.find({ user: userId })
+    .populate({ path: "products.product", select: "images name  price" })
+    .then((orders) => {
+      res.status(200).json(orders);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+module.exports = router;
