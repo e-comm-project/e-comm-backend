644eb52264a353377c27932c937110773927ef85
Richard Hadzhiev <ri4koto100@gmail.com>
2024-05-29 10:39:49 +0200

    implement Order model

---
+++ models/Order.model.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const orderSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        priceAtPurchase: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    total: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Order", orderSchema);
