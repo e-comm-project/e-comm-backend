0b968fc871a03f592c68543a2e1d25b86e4c94ee
Richard Hadzhiev <ri4koto100@gmail.com>
2024-06-03 12:10:38 +0200

    add image to user model

--- models/User.model.js
+++ models/User.model.js
@@ -23,6 +23,11 @@ const userSchema = new Schema(
       enum: ["user", "admin"],
       default: "user",
     },
+    image: {
+      type: String,
+      default:
+        "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg",
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
