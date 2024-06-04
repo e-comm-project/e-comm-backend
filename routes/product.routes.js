2f8b40838352b61623f6b262f22d8b1caef09882
kafdhali <khaledafdh075@gmail.com>
2024-06-04 17:36:24 +0200

    feat: Add route to retrieve product by ID

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -110,5 +110,14 @@ router.delete("/products/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
-
+router.get("/product/:id", (req, res) => {
+  const id = req.params.id;
+  Product.findById(id)
+    .then((product) => {
+      res.status(200).json(product);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 module.exports = router;
